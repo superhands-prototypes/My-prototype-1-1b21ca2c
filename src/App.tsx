fd4c660fb0ddcbd1664283194e5fab32f3044a53
grant+123test <grant+123test@superhands.ai>
2026-01-19 14:55:10 +0000

    Update from Superhands

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,7 @@
 export default function App() {
   return (
     <div className="app">
-      <div className="animated-bg">
-        <div className="gradient-orb orb-1"></div>
-        <div className="gradient-orb orb-2"></div>
-        <div className="gradient-orb orb-3"></div>
-        <div className="gradient-orb orb-4"></div>
-        <div className="gradient-orb orb-5"></div>
-        <div className="particle"></div>
-        <div className="particle"></div>
-        <div className="particle"></div>
-        <div className="particle"></div>
-        <div className="particle"></div>
-        <div className="particle"></div>
-        <div className="particle"></div>
-        <div className="particle"></div>
-        <div className="particle"></div>
-        <div className="particle"></div>
-      </div>
+      <h1 className="hello-text">HELLO!</h1>
     </div>
   )
 }
